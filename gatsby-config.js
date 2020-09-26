49deb1e05eb8f75e93f592a5d2f19b341c4776e6
weakow <w@weakow.com>
2020-09-27 05:11:40 +0800

    Basic config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful starter",
+    title: "Headless CMS 无头中文站",
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
